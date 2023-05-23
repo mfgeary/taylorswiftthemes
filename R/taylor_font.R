9ba9c5cc52d80238f958ccd0e2934b2db3202fa0
Marion Geary <77766742+mfgeary@users.noreply.github.com>
2023-05-22 23:23:16 -0400

    :wink: Add list of fonts for albums

---
+++ R/taylor_font.R
@@ -0,0 +1,23 @@
+#' Complete list of palettes
+#'
+#' Use \code{\link{taylor_fonts}} to get the album's font.
+#'
+#' @export
+taylor_font <- list(
+  debut = "Satisfaction",
+  fearless = "Montserrat",
+  fearless_TV = "Montserrat",
+  speaknow = "Rochester",
+  speak_now = "Rochester",
+  speak_now_TV = "Rochester",
+  speaknow_TV = "Rochester",
+  speaknowTV = "Rochester",
+  red = "Anton",
+  red_TV = "Anton",
+  nineteen_eighty_nine = "Permanent Marker",
+  reputation = "UnifrakturMaguntia",
+  lover = "Satisfy",
+  folklore = "IM Fell DW Pica",
+  evermore = "IM Fell DW Pica",
+  midnights = "Helvetica"
+)
